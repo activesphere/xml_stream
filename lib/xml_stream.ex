a080fa230dae9a86c1b28e9c7529906d3fec695b
Rohit <shinde.rohitt@gmail.com>
2017-11-09 12:42:26 +0530

    Keep track of tree depth, use for pretty print

--- lib/xml_stream.ex
+++ lib/xml_stream.ex
@@ -9,14 +9,40 @@ defmodule XmlStream do
     [{:const, value}]
   end
 
-  def stream(node) do
+  def stream_builder(node) do
     Stream.transform(node, [], fn i, acc ->
       if is_tuple(i) do
-        {[print(i)], acc}
+        {[i], acc}
       else
-        {stream(i), acc}
+        {stream_builder(i), acc}
       end
     end)
   end
 
+  def stream(node) do
+    nodes_stream = stream_builder(node)
+    Stream.transform(nodes_stream, [], fn i, acc ->
+      {acc, level} = update_acc(acc, i)
+      {[print(i, level)], acc}
+    end)
+  end
+
+  defp update_acc(acc, i) do
+    if acc == [] do
+      {[i] ++ acc, 0}
+    else
+      curr_type = elem(i, 0)
+      curr_name = elem(i, 1)
+      prev_name = elem(hd(acc), 1)
+
+      cond do
+        curr_type == :const ->
+          {acc, length(acc)}
+        curr_type == :close && curr_name == prev_name ->
+          {tl(acc), length(acc) - 1}
+        true ->
+          {[i] ++ acc, length(acc)}
+      end
+    end
+  end
 end
