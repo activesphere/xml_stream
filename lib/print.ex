29a6bb6ed94943ff8cda100b304c5dbd87bc1b62
Rohit <shinde.rohitt@gmail.com>
2017-11-09 17:59:35 +0530

    wip, move update_acc to print.ex

--- lib/print.ex
+++ lib/print.ex
@@ -1,5 +1,7 @@
 defmodule XmlStream.Print do
 
+
+
   defmodule Pretty do
     #TODO: escape according to spec
     def print({:open, name, attrs}) do
@@ -17,12 +19,34 @@ defmodule XmlStream.Print do
       [value, "\n"]
     end
 
-    def print(node, level) do
-      [indent(level)] ++ print(node)
+    def print(node, acc) do
+      acc = update_acc(node, acc)
+      level = elem(acc, 1)
+      {[indent(level), print(node)], acc}
     end
 
     defp indent(level, indent_with \\ "\t") do
       String.duplicate(indent_with, level)
+    end
+
+    defp update_acc(curr, {prev, level}) do
+      if prev == [] do
+        {curr, 0}
+      else
+        curr_type = elem(curr, 0)
+        curr_name = elem(curr, 1)
+        prev_name = elem(prev, 1)
+        cond do
+          curr_type == :const ->
+            {prev, level + 1}
+          curr_type == :close ->
+            {curr, level - 1}
+          curr_type == :open && curr_name == prev_name ->
+            {curr, level}
+          true ->
+            {curr, level + 1}
+        end
+      end
     end
   end
 
--- lib/xml_stream.ex
+++ lib/xml_stream.ex
@@ -1,5 +1,4 @@
 defmodule XmlStream do
-
   def element(name, attrs, body) do
     Stream.concat([[{:open, name, attrs}], body, [{:close, name}]])
   end
@@ -23,29 +22,7 @@ defmodule XmlStream do
     nodes_stream = stream_builder(node)
     acc = {[], 0}
     Stream.transform(nodes_stream, acc, fn i, acc ->
-      acc = update_acc(i, acc)
-      level = elem(acc, 1)
-      {[printer.print(i, level)], acc}
+      printer.print(i, acc)
     end)
-  end
-
-  defp update_acc(curr, {prev, level}) do
-    if prev == [] do
-      {curr, 0}
-    else
-      curr_type = elem(curr, 0)
-      curr_name = elem(curr, 1)
-      prev_name = elem(prev, 1)
-      cond do
-        curr_type == :const ->
-          {prev, level + 1}
-        curr_type == :close ->
-          {curr, level - 1}
-        curr_type == :open && curr_name == prev_name ->
-          {curr, level}
-        true ->
-          {curr, level + 1}
-      end
-    end
   end
 end
