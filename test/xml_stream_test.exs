73a52c9a331b3310227fbb3b41c630f7712d05fe
Rohit <shinde.rohitt@gmail.com>
2017-11-15 16:10:44 +0530

    Add tests for (pretty & dumb) printers

--- test/xml_stream_test.exs
+++ test/xml_stream_test.exs
@@ -4,52 +4,44 @@ defmodule XmlStreamTest do
   import XmlStream
   import SweetXml, only: [sigil_x: 2, xpath: 2, parse: 1]
 
-  def assert_xpath(doc, path, expected) do
-    assert xpath(doc, path) == expected
+  test "Pretty Print" do
+    expected_pretty = ~s(<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n) <>
+      ~s(<workbook date=\"false\"/>\n<sheet>\n\t<row/>\n\t<row>\n\t\t) <>
+      ~s(<cell foo=\"bar\">1</cell>\n\t\t<cell/>\n\t</row>\n\t<row>\n\t\t) <>
+      ~s(<cell/>\n\t\t<cell foo=\"bar\">1</cell>\n\t</row>\n\t<row/>\n</sheet>)
+
+    assert expected_pretty == pretty_out()
   end
 
-  test "node" do
-    rows = Stream.map(1..2, fn i ->
-      cells = Stream.map(1..2, fn j ->
-        element("cell", %{row: to_string(i), column: to_string(j)}, content(to_string(i)))
-      end)
-      element("row", cells)
-    end)
+  test "Ugly Print" do
+    expected_ugly = ~s(<?xml version="1.0" encoding="UTF-8"?>) <>
+      ~s(<workbook date="false"/><sheet><row/><row><cell foo="bar">1</cell>) <>
+      ~s(<cell/></row><row><cell/><cell foo="bar">1</cell></row><row/></sheet>)
 
-    options = [
-      printer: XmlStream.Print.Pretty
-    ]
-
-    stream([declaration(), empty_element("workbook", %{date: "false"}), element("sheet", rows)], options)
-    |> Stream.each(fn item ->
-      IO.write item
-    end)
-    |> Stream.run
+    assert expected_ugly == ugly_out()
   end
 
-  test "escape" do
-    rows = Stream.map(1..2, fn i ->
-      cells = Stream.map(1..2, fn _ ->
-        attrs = %{prop1: "'foo", prop2: "bar\"", prop3: "baz&", prop4: ">"}
-        content_text = case i do
-                         1 -> "&<"
-                         2 -> "<&"
-                         true -> ""
-                       end
-        element("cell", attrs, content(content_text <> to_string(i)))
-      end)
-      element("row", cells)
-    end)
-
-    options = [
-      printer: XmlStream.Print.Ugly
+  test "Escapes" do
+    common_attrs = %{prop1: "'foo", prop2: "bar\"", prop3: "baz&", prop4: ">"}
+    doc = [
+      declaration(),
+      element("sheet",
+        [
+          element("row",
+            [
+              element("cell", common_attrs, content("&<1")),
+              element("cell", common_attrs, content("1")),
+            ]),
+          element("row",
+            [
+              element("cell", common_attrs, content("<&2")),
+              element("cell", common_attrs, content("<&2")),
+            ]),
+        ]
+      )
     ]
 
-    xml_string = stream([declaration(), element("sheet", rows)], options)
-    |> Enum.to_list()
-    |> Enum.join("")
-
-    doc = parse(xml_string)
+    doc = parse(doc_string(doc))
     # First row text, should be '&<1'
     assert_xpath(doc, ~x"//sheet/row/cell/text()", '&<1')
 
@@ -61,5 +53,43 @@ defmodule XmlStreamTest do
     assert_xpath(doc, ~x"//sheet/row/cell/@prop2", 'bar"')
     assert_xpath(doc, ~x"//sheet/row/cell/@prop3", 'baz&')
     assert_xpath(doc, ~x"//sheet/row/cell/@prop4", '>')
+  end
+
+  @sample_xml [
+    declaration(),
+    empty_element("workbook", %{date: "false"}),
+    element("sheet",
+      [empty_element("row"),
+       element("row",
+         [
+           element("cell", %{foo: "bar"}, content("1")),
+           empty_element("cell")
+         ]),
+       element("row",
+         [
+           empty_element("cell"),
+           element("cell", %{foo: "bar"}, content("1")),
+         ]),
+       empty_element("row")
+      ]
+    )
+  ]
+
+  defp doc_string(elem_stream, options \\ [printer: XmlStream.Print.Ugly]) do
+    stream(elem_stream, options)
+    |> Enum.to_list
+    |> Enum.join("")
+  end
+
+  defp pretty_out() do
+    doc_string(@sample_xml, [printer: XmlStream.Print.Pretty])
+  end
+
+  defp ugly_out() do
+    doc_string(@sample_xml)
+  end
+
+  defp assert_xpath(doc, path, expected) do
+    assert xpath(doc, path) == expected
   end
 end
