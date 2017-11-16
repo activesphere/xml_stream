1427a208e0fc07165c4d9f6ab50eb9820946d776
Anantha Kumaran <ananthakumaran@gmail.com>
2017-11-16 17:23:48 +0530

    make it generic for now, the types might change

--- lib/xml_stream/printer/printer.ex
+++ lib/xml_stream/printer/printer.ex
@@ -1,10 +1,5 @@
 defmodule XmlStream.Printer do
-  @type element_types :: :open | :const | :close
-  @type attrs :: %{required(String.t) => String.t}
-  @type element :: {element_types, String.t, attrs}
-  @type acc :: {element, number}
-
-  @callback print(element, acc | nil) :: {[String.t], acc | nil}
+  @callback print(term, term) :: {iodata, term}
   @callback init(term) :: term
 
 
